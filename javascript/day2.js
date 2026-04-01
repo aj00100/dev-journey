b986f5782529b720ee5d71c8e83e05ce4e6604dc
aj00100 <ajay2026kr@gmail.com>
2026-04-01 12:03:03 +0530

    day2: datatypes

---
+++ javascript/day2.js
@@ -0,0 +1,9 @@
+const name="Ajay"
+let age=20 
+let city = "Bhopal"
+let cgpa = 8.5
+let course = "CSE CYBERSECURITY"
+let isEnrolled = true
+console.log("Student Profile\n-----------------")
+console.log(`Name: ${name}\nAge: ${age}\nCity: ${city}\nCourse: ${course}\nEnrolled: ${isEnrolled}\nCGPA: ${cgpa}\n-----------------`)
+console.log(`Type of age: ${typeof age}\nType of isEnrolled: ${typeof isEnrolled}\nType of cgpa: ${typeof cgpa}`)
